5bc5a36037b34f612be7454d825472265f418dbe
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2023-01-04 11:04:34 +0200

    Update Graph_7.py

--- pages/Graph_7.py
+++ pages/Graph_7.py
@@ -14,9 +14,10 @@ image = np.array(im)
 st.image(image)
 
 
-st.markdown(" <center>  <h1> KPC (DRLG/WO) Drops Analysis by Location </h1> </font> </center> </h1> ",
+
+st.markdown(" <center>  <h1> KPC (DRLG/WO) Drops Analysis (Detailed Location Pass Points with Rigs Contribution)</h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
- 
+##### 
 
 #####
 
